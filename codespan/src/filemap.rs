a3d7cd3907678ac15a49849f92d30d65fdf92f03
Markus Westerlind <marwes91@gmail.com>
2018-04-12 08:29:52 +0200

    Expose FileMap::new, remove FileMap::anonymous

--- codespan/src/filemap.rs
+++ codespan/src/filemap.rs
@@ -103,12 +103,12 @@ pub struct FileMap {
 }
 
 impl FileMap {
-    pub fn anonymous(src: String) -> FileMap {
-        Self::new(FileName::virtual_(""), src, ByteIndex::from(1))
+    /// Construct a new filemap, creating an index of line start locations
+    pub fn new(name: FileName, src: String) -> FileMap {
+        FileMap::with_index(name, src, ByteIndex(1))
     }
 
-    /// Construct a new filemap, creating an index of line start locations
-    pub(crate) fn new(name: FileName, src: String, start: ByteIndex) -> FileMap {
+    pub(crate) fn with_index(name: FileName, src: String, start: ByteIndex) -> FileMap {
         use std::iter;
 
         let span = ByteSpan::from_offset(start, ByteOffset::from_str(&src));
@@ -138,7 +138,7 @@ impl FileMap {
         let mut src = String::new();
         file.read_to_string(&mut src)?;
 
-        Ok(FileMap::new(FileName::Real(name), src, start))
+        Ok(FileMap::with_index(FileName::Real(name), src, start))
     }
 
     /// The name of the file that the source came from
